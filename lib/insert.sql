401e0befa153cc9527896ba0df6dfcd3ee28a689
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-03-03 17:42:59 -0500

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO bears(name, age, sex, color, temperament, alive)
+-- VALUES ('Mr Chocolate', 2, 'M', 'black', 'calm', True)
+
+
+
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ('Unnamed', 7, 'F', 'NULL', 'sweet', True)
+
+
